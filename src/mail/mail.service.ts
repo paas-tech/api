c1ef8c8774a974f768f1cb648f3bc1ebfe5f68eb
BioTheWolff <biothewolff@gmx.fr>
2023-06-26 20:39:39 +0200

    feat(mail): implement service for user confirmation email

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { _InternalMailerService as MailerService } from 'src/utils/mailer.internal.service';
+import { compile } from 'handlebars';
+
+const EMAIL_CONFIRMATION_TEMPLATE = `<h2>Welcome to PaasTech!</h2>
+<p>Please click the link below to confirm your email:</p>
+<p>
+    <a href="{{ url }}">Confirm</a>
+</p>
+
+<p>If you have trouble opening the link, you can copy the following URL into your browser: </p>
+<p>{{ url }}</p>
+
+<p>If you did not request this email you can safely ignore it.</p>`;
 
 @Injectable()
-export class MailService {}
+export class MailService {
+    constructor(private mailerService: MailerService) {}
+
+    async sendUserConfirmation(user: User, token: string) {
+        const url = `example.com/auth/confirm?token=${token}`;
+        const template = compile(EMAIL_CONFIRMATION_TEMPLATE);
+    
+        await this.mailerService.sendMail({
+            to: user.email,
+            subject: 'Welcome to PaasTech!',
+            html: template({url}),
+
+            // TODO: use @nestjs-modules/mailer once bumped to 10.0.0
+
+            //template: './confirmation',
+            //context: {
+            //    url,
+            //},
+        });
+    }
+
+}
