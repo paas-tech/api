370417a1d5ae66d3fd0920be9134f16277c5c25e
DziyanaT <dziyana.tsetserava@etu.umontpellier.fr>
2023-07-02 20:00:14 +0200

    feat: use passport-jwt for authentication

---
+++ src/auth/strategies/jwt-strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ITokenPayload } from '../interfaces/tokenPayload';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any): Promise<ITokenPayload> {
+    return { sub: payload.sub, username: payload.username, isAdmin: payload.isAdmin };
+  }
+}
