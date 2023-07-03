26ead76842de35de05f237e9e6c7d6198a6ed39c
Muriel Paraire <muriel.paraire@etu.umontpellier.fr>
2023-07-03 18:55:44 +0200

    feat: mailservice for password request

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
 import { _InternalMailerService as MailerService } from './mailer.internal.service';
 import { compile } from 'handlebars';
 import { ConfigService } from '@nestjs/config';
@@ -15,6 +14,17 @@ const EMAIL_CONFIRMATION_TEMPLATE = `<h2>Welcome to PaasTech!</h2>
 
 <p>If you did not request this email you can safely ignore it.</p>`;
 
+const PASSWORD_RESET_TEMPLATE = `<h2>Password reset</h2>
+<p>Please click the link below to reset your password:</p>
+<p>
+    <a href="{{ url }}">Confirm</a>
+</p>
+
+<p>If you have trouble opening the link, you can copy the following URL into your browser: </p>
+<p>{{ url }}</p>
+
+<p>If you did not request this email please contact us immediatly.</p>`;
+
 @Injectable()
 export class MailService {
 
@@ -27,14 +37,37 @@ export class MailService {
         this.hostname = `${this.configService.getOrThrow('APP_HOST')}:${this.configService.getOrThrow('APP_PORT')}`;
     }
 
-    async sendUserConfirmation(user: User, token: string) {
+    async sendUserConfirmation(email: string, token: string) {
         const url = `${this.hostname}/auth/confirm?token=${token}`;
         const template = compile(EMAIL_CONFIRMATION_TEMPLATE);
-    
-        await this.mailerService.sendMail({
-            to: user.email,
+
+        return await this.mailerService.sendMail({
+            from: `${this.configService.getOrThrow('MAILER_FROM')}`,
+            to: email,
             subject: 'Welcome to PaasTech!',
-            html: template({url}),
+            html: template({ url }),
+
+            // TODO: use @nestjs-modules/mailer once bumped to 10.0.0
+
+            //template: './confirmation',
+            //context: {
+            //    url,
+            //},
+        }).then(() => {
+            return true;
+        }).catch((err: any) => {
+            return false;
+        });
+    }
+
+    async sendPasswordReset(email: string, token: string) {
+        const url = `${this.hostname}/auth/pwreset?token=${token}`;
+        const template = compile(PASSWORD_RESET_TEMPLATE);
+
+        await this.mailerService.sendMail({
+            to: email,
+            subject: 'Password reset PaasTech !',
+            html: template({ url }),
 
             // TODO: use @nestjs-modules/mailer once bumped to 10.0.0
 
