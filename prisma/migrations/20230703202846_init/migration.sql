d1f0451e193c6a425fe59087138d47739222bcf5
BioTheWolff <biothewolff@gmx.fr>
2023-07-03 22:29:13 +0200

    fix(prisma)!: regenerate init migration and squash them
    BREAKING CHANGE

--- prisma/migrations/20230703202846_init/migration.sql
+++ prisma/migrations/20230703202846_init/migration.sql
@@ -1,13 +1,14 @@
 -- CreateTable
 CREATE TABLE "users" (
-    "id" SERIAL NOT NULL,
+    "id" UUID NOT NULL,
     "username" VARCHAR(40) NOT NULL,
     "email" VARCHAR(100) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
     "is_admin" BOOLEAN NOT NULL,
     "email_nonce" VARCHAR(255),
+    "password_nonce" VARCHAR(255),
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3),
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
@@ -17,33 +18,31 @@ CREATE TABLE "ssh_keys" (
     "id" SERIAL NOT NULL,
     "value" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
-    "user_id" INTEGER NOT NULL,
+    "updated_at" TIMESTAMP(3),
+    "user_id" UUID NOT NULL,
 
     CONSTRAINT "ssh_keys_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "projects" (
-    "id" SERIAL NOT NULL,
-    "uuid" UUID NOT NULL,
+    "id" UUID NOT NULL,
     "name" VARCHAR(40) NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
-    "user_id" INTEGER NOT NULL,
+    "updated_at" TIMESTAMP(3),
+    "user_id" UUID NOT NULL,
 
     CONSTRAINT "projects_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "deployments" (
-    "id" SERIAL NOT NULL,
-    "uuid" UUID NOT NULL,
+    "id" UUID NOT NULL,
     "name" VARCHAR(40) NOT NULL,
     "config" JSONB NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
-    "project_id" INTEGER NOT NULL,
+    "updated_at" TIMESTAMP(3),
+    "project_id" UUID NOT NULL,
 
     CONSTRAINT "deployments_pkey" PRIMARY KEY ("id")
 );
@@ -53,21 +52,6 @@ CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
-
--- CreateIndex
-CREATE UNIQUE INDEX "ssh_keys_user_id_key" ON "ssh_keys"("user_id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "projects_uuid_key" ON "projects"("uuid");
-
--- CreateIndex
-CREATE UNIQUE INDEX "projects_user_id_key" ON "projects"("user_id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "deployments_uuid_key" ON "deployments"("uuid");
-
--- CreateIndex
-CREATE UNIQUE INDEX "deployments_project_id_key" ON "deployments"("project_id");
 
 -- AddForeignKey
 ALTER TABLE "ssh_keys" ADD CONSTRAINT "ssh_keys_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
