e8f39186a2b34520bb209e8dbca5120ba8de604b
DziyanaT <dziyana.tsetserava@etu.umontpellier.fr>
2023-07-05 12:11:46 +0200

    fix: remove unused uuid imports, search/delete project by id instead of uuid

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { Project, User } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { CreateProjectDto } from './dto/create-project.dto';
-import { v4 as uuidv4 } from 'uuid';
 import { exclude } from 'src/utils/prisma-exclude';
 import { SanitizedProject } from './types/sanitized-project.type';
 
@@ -18,16 +17,15 @@ export class ProjectsService {
     return this.sanitizeOutput(await this.prisma.project.create({
       data: {
         name: project.name,
-        uuid: uuidv4(),
         userId: user.id,
       }
     }));
   }
 
-  async findOne(uuid: string): Promise<SanitizedProject|null> {
+  async findOne(id: string): Promise<SanitizedProject|null> {
     const project = await this.prisma.project.findUnique({
       // FIXME: ensure one can only find their projects and not somebody else's
-      where: {uuid},
+      where: {id},
     });
 
     if (project) {
@@ -37,10 +35,10 @@ export class ProjectsService {
     }
   }
 
-  async delete(uuid: string) {
+  async delete(id: string) {
     // FIXME: ensure the requesteer has the rights to delete the project
     await this.prisma.project.delete({
-      where: {uuid}
+      where: {id}
     });
   }
 }
