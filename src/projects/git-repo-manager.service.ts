ad6c9bb7dd71ef24c5afc30daaf50e6c726e4f3a
Mauran <thomas.mauran@etu.umontpellier.fr>
2023-07-06 20:23:14 +0200

    feat(projects): working fine

---
+++ src/projects/git-repo-manager.service.ts
@@ -0,0 +1,41 @@
+import { HttpException, Injectable, OnModuleInit } from '@nestjs/common';
+import { Client, ClientGrpc } from '@nestjs/microservices';
+import { GitRepoManagerClient, RepositoryRequest, RepositoryResponse } from 'paastech-proto/types/proto/git-repo-manager';
+import { firstValueFrom } from 'rxjs';
+import { grpcClientOptions } from 'src/utils/grpc/grpc-client.options';
+
+@Injectable()
+export class GitRepoManagerService implements OnModuleInit {
+  @Client(grpcClientOptions)
+  private readonly client: ClientGrpc;
+  private gitRepoManager: GitRepoManagerClient;
+
+  onModuleInit(): void {
+    this.gitRepoManager = this.client.getService<GitRepoManagerClient>('GitRepoManager');
+  }
+
+  async create(request: RepositoryRequest): Promise<RepositoryResponse> {
+    try {
+      console.log(request);
+      return await firstValueFrom(this.gitRepoManager.createRepository(request));
+    } catch (error) {
+      if (error.code === 6) {
+        throw new HttpException('Repository already exists', 409);
+      } else {
+        throw new HttpException(`Internal server error: ${error}`, 500);
+      }
+    }
+  }
+
+  async delete(request: RepositoryRequest): Promise<RepositoryResponse> {
+    try {
+      return await firstValueFrom(this.gitRepoManager.deleteRepository(request));
+    } catch (error) {
+      if (error.code === 5) {
+        throw new HttpException(`Repository not found: `, 404);
+      } else {
+        throw new HttpException('Internal server error', 500);
+      }
+    }
+  }
+}
--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,51 +1,76 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Post } from '@nestjs/common';
-import { AdminOnly } from 'src/auth/decorators/adminonly.decorator';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { ProjectsService } from './projects.service';
 import { SanitizedProject } from './types/sanitized-project.type';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { GetUser } from 'src/auth/decorators/user.decorator';
+import { UserDecoratorType } from 'src/auth/types/user-decorator.type';
+import { PrismaService } from 'src/prisma.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
-@ApiBearerAuth()
-@ApiTags('projects')
 @Controller('projects')
 export class ProjectsController {
-    constructor(private projectsService: ProjectsService) {}
+  constructor(
+    private projectsService: ProjectsService,
+    private prisma: PrismaService,
+  ) {}
 
-    // GET /projects
-    // This action returns all of the authenticated user's projects
-    @AdminOnly()
-    @Get()
-    @HttpCode(501)
-    async findAll() {
-        // TODO: implement and remove the 501 decorator above
-        return '501 Not Implemented';
-    }
+  // GET /projects
+  // This action returns all of the authenticated user's projects
+  @Get()
+  async findAll(@GetUser() user: UserDecoratorType) {
+    return this.projectsService.findAll(user.sub);
+  }
 
-    // GET /projects/:uuid
-    // This action returns a #${id} project;
-    @Get(':uuid')
-    async findOne(@Param('uuid') id: string): Promise<SanitizedProject> {
-        return this.projectsService.findOne(id);
-    }
+  // GET /projects/:uuid
+  // This action returns a #${id} project;
+  @Get(':uuid')
+  async findOne(
+    @Param('uuid') id: string,
+    @GetUser() user: UserDecoratorType,
+  ): Promise<SanitizedProject> {
+    return this.projectsService.findOne(id, user.sub);
+  }
 
-    // POST /projects
-    /*
+  // POST /projects
+  /*
         This action creates a project
         and returns a NewProject Object
         with the project's id and name
     */
-    @Post()
-    @HttpCode(501)
-    async create(@Body() createProjectDto: CreateProjectDto) {
-        // TODO: implement and remove the 501 decorator
-        return '501 Not Implemented';
-    }
+  @Post()
+  @UsePipes(new ValidationPipe())
+  async create(
+    @Body() request: CreateProjectDto,
+    @GetUser() user: UserDecoratorType,
+  ): Promise<SanitizedProject> {
+    const createdProject = await this.projectsService.create(
+      user.sub,
+      request.name,
+    );
+    return createdProject;
+  }
 
-    // DELETE /projects/:uuid
-    // This action deletes a #${id} project
-    @Delete(':uuid')
-    async delete(@Param('uuid') uuid: string) {
-        this.projectsService.delete(uuid);
-        return `OK`;
-    }
+  // DELETE /projects/:uuid
+  // This action deletes a #${id} project
+  @Delete(':uuid')
+  @UseGuards(JwtAuthGuard)
+  async delete(
+    @Param('uuid') uuid: string,
+    @GetUser() user: UserDecoratorType,
+  ): Promise<SanitizedProject> {
+    // Delete the project from the database
+    const project = await this.projectsService.delete(uuid, user.sub);
+
+    return project;
+  }
 }
