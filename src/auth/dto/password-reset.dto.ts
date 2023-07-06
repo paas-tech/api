5b0dd90900b03a592b738e3571af3cdb134f958b
Muriel Paraire <muriel.paraire@etu.umontpellier.fr>
2023-07-06 11:43:26 +0200

    fix: added constraints to password verification

--- src/auth/dto/password-reset.dto.ts
+++ src/auth/dto/password-reset.dto.ts
@@ -12,6 +12,11 @@ export class PasswordResetDto {
     password: string;
 
     @IsNotEmpty()
+    @IsByteLength(0, 72)
+    @MaxLength(72)
+    @Matches(PASSWORD_REGEX, {
+        message: 'Password should be minimum eight character long and have at least one uppercase letter, one lowercase letter, one number and one special character.'
+    })
     @MatchesWithProperty(PasswordResetDto, o => o.password)
     passwordVerification: string;
 }
