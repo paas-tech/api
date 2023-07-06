f0846ea3172c5fb5ffc1bb31954588e3484d489f
Muriel Paraire <muriel.paraire@etu.umontpellier.fr>
2023-07-06 11:41:33 +0200

    fix: password verification on reset

--- src/auth/dto/password-reset.dto.ts
+++ src/auth/dto/password-reset.dto.ts
@@ -1,10 +1,14 @@
-import{ IsByteLength, IsNotEmpty, MaxLength } from 'class-validator';
+import{ IsByteLength, IsNotEmpty, Matches, MaxLength } from 'class-validator';
+import { PASSWORD_REGEX } from 'src/utils/constants';
 import { MatchesWithProperty } from 'src/validators/match-other.validator';
 
 export class PasswordResetDto {
     @IsNotEmpty()
     @IsByteLength(0, 72)
     @MaxLength(72)
+    @Matches(PASSWORD_REGEX, {
+        message: 'Password should be minimum eight character long and have at least one uppercase letter, one lowercase letter, one number and one special character.'
+    })
     password: string;
 
     @IsNotEmpty()
