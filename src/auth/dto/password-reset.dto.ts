f62d77390ac72c92c7e3086b96b67e13737aa5be
BioTheWolff <biothewolff@gmx.fr>
2023-07-06 08:50:54 +0200

    feat: add match other validator and its use in password reset

--- src/auth/dto/password-reset.dto.ts
+++ src/auth/dto/password-reset.dto.ts
@@ -1,9 +1,11 @@
-import{ IsEmail, IsNotEmpty } from 'class-validator';
+import{ IsNotEmpty } from 'class-validator';
+import { MatchesWithProperty } from 'src/validators/match-other.validator';
 
 export class PasswordResetDto {
     @IsNotEmpty()
     password: string;
 
     @IsNotEmpty()
+    @MatchesWithProperty(PasswordResetDto, o => o.password)
     passwordVerification: string;
 }
