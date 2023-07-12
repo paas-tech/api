382d6d1c75f90dbcaaf0b85208165a1e39419a62
BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>
2023-07-12 12:10:22 +0200

    feat(logger): add logger to mail

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { _InternalMailerService } from './mailer.internal.service';
+import { CustomLoggerService } from 'src/logger/custom-logger.service';
 
 @Global()
 @Module({
-  providers: [MailService, _InternalMailerService],
+  providers: [MailService, _InternalMailerService, CustomLoggerService],
   exports: [MailService],
 })
 export class MailModule {}
