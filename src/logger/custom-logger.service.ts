c243ea9c8f0f5559a343fc5e0f615473fe579ebc
BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>
2023-07-12 11:56:49 +0200

    fix(logger): display the full error

--- src/logger/custom-logger.service.ts
+++ src/logger/custom-logger.service.ts
@@ -3,6 +3,7 @@ import { ConsoleLogger, Injectable, Scope } from '@nestjs/common';
 @Injectable({ scope: Scope.TRANSIENT })
 export class CustomLoggerService extends ConsoleLogger {
   cError(method: string, message: string, err: unknown): void {
-    super.error(`{${method}} : ${message}: ${String(err)}`);
+    super.error(`{${method}} : ${message}`);
+    super.error(err); // display the full error after so we dont lose data
   }
 }
